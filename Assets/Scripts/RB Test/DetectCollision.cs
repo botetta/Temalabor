12d4de4cef05bb221d28bc30ee97e58c47abd388
Hölbling Botond <botetta@gmail.com>
2023-11-24 18:00:21 +0100

    checkpoints, deadly cubes

--- Assets/Scripts/RB Test/DetectCollision.cs
+++ Assets/Scripts/RB Test/DetectCollision.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class DetectCollision : MonoBehaviour
 {
+    PlayerMovementScript playerMovementScript;
+    private void Start()
+    {
+        // Get the reference to the parent game object of the Capsule, which is the First Person Player
+        GameObject firstPersonPlayer = transform.parent.gameObject;
+
+        // Get the reference to PlayerMovementScript using GetComponent
+        playerMovementScript = firstPersonPlayer.GetComponent<PlayerMovementScript>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "MovingPlatform")
         {
             print("Player is touching moving platform");
+            playerMovementScript.OnDeath();
         }
     }
+
+
 }
